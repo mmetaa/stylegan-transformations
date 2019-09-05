8862dc2de587d99affdfdd49e3f1e2e2cff8b7ed
mmetaa <54929396+mmetaa@users.noreply.github.com>
2019-09-04 23:46:43 -0700

    Update encode_images.py

--- encode_images.py
+++ encode_images.py
@@ -70,8 +70,8 @@ def main():
         generated_dlatents = generator.get_dlatents()
         for img_array, dlatent, img_name in zip(generated_images, generated_dlatents, names):
             img = PIL.Image.fromarray(img_array, 'RGB')
-            img.save(os.path.join(args.generated_images_dir, f'{img_name}.png'), 'PNG')
-            np.save(os.path.join(args.dlatent_dir, f'{img_name}.npy'), dlatent)
+            img.save(os.path.join(args.generated_images_dir, f'{img_name}_{args.iterations}.png'), 'PNG')
+            np.save(os.path.join(args.dlatent_dir, f'{img_name}_{args.iterations}.npy'), dlatent)
 
         generator.reset_dlatents()
 
